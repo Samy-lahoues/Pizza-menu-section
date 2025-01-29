af42f71658972fe161dffc4d2bdba83b277b51b5
Samy-lahoues <SamyLahoues@outlook.com>
2025-01-28 18:37:14 -0600

    feat: Add sub components and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Fragment ,ReactElement } from "react";
 import "./App.css"
 
 const pizzaData = [
@@ -63,54 +63,86 @@ function Header() {
     )
 }
 function Menus() {
+    // const pizzaData : [] = [];
+    const numPizza : number = pizzaData.length;
     
     return (
         <div className='container'>
             <div className="menu">
                 <h2>OUR MENU</h2>
-                <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious</p>
-                <div className='pizzas'>
-                {pizzaData.map((pizza) => (
-                    <Pizza
-                    className={pizza.soldOut ? "pizza sold-out" : "pizza"}
-                    name={pizza.name}
-                    ingredients={pizza.ingredients}
-                    price={pizza.price}
-                    photoName={pizza.photoName}
-                    />
-                ))}
-                </div>
-                <div className="order">
-                    <p>we're open until 22:00, Come visit us or order online</p>
-                    <button className='btn'>Order Now</button>
-                </div>
+                {numPizza > 0 ? (
+                    <Fragment key={"go"}>
+                        <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious</p>
+                        <ul className='pizzas'>
+                        {pizzaData.map((pizza) => (
+                            <Pizza
+                            className={`pizza ${pizza.soldOut && "sold-out"}`}
+                            name={pizza.name}
+                            ingredients={pizza.ingredients}
+                            price={pizza.price}
+                            photoName={pizza.photoName}
+                            soldOut={pizza.soldOut}
+                            />
+                        ))}
+                        </ul>
+                    </Fragment>
+                ) : (
+                    <p>We're still working on our menu, Please come back later :)</p>
+                )}
             </div>
         </div>
     )
 }
 function Footer() {
+    const openHour : number = 8;
+    const closeHour : number = 22;
+    const currentHour : number = new Date().getHours()
+    const isOpen : boolean = (currentHour > openHour && currentHour < closeHour) 
+
     return (
         <div className='footer'>
+            {  isOpen ? (
+            <Order isOpen={isOpen} closeHour={closeHour} />
+            ): (
+                <Order isOpen={isOpen} openHour={openHour} closeHour={closeHour} />
+            )}
         </div>
     )
+}
+interface orderProps {
+    closeHour? : number,
+    openHour?: number,
+    isOpen: boolean
+}
+const Order = ({openHour, closeHour, isOpen} : orderProps) => {
+    const openText : ReactElement = <p>{`We're open until ${closeHour}:00`}, Come visit us or order online</p>
+    const closedText : ReactElement = <p>{`We're happy welcome you between ${openHour}:00 and ${closeHour}:00`}</p>
+    return (
+        <div className="order">
+            {isOpen ? openText : closedText}
+            <button className="btn">Order Now</button>
+        </div>
+    );
 }
 interface pizzaProps{
     name: string,
     ingredients: string,
     price: number,
     photoName: string,
-    className?: string
+    className?: string,
+    soldOut?: boolean
 }
-function Pizza({name, ingredients, price, photoName, className} : pizzaProps) {
+function Pizza({name, ingredients, price, photoName, className, soldOut} : pizzaProps) {
+    // if (soldOut) return null;
     return (
-        <div className={className}>
+        <li className={className}>
             <img alt={"Pizza spinachi"} src={"../public/" + photoName} />
             <div>
                 <h3>{name}</h3>
                 <p>{ingredients}</p>
-                <span>{price}</span>
+                <span>{soldOut ? "Sold out" : price}</span>
             </div>
-        </div>
+        </li>
     )
 }
 export default App;
